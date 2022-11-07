bacdb69955f047a9fbff67118b1c7d4021d94476
cmunoslosa <cmunoslosa@hawk.iit.edu>
2022-11-06 19:23:02 -0600

    musical data sript added

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,103 +1,25 @@
 from general_data_gatherer import DataGatherer
 import pandas as pd
 import os
-from sklearn.decomposition import PCA  # to apply PCA
-from numpy import array
-from numpy import argmax
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 import json
-
-x = DataGatherer("data/MillionSongSubset/A/A/A/TRAAAAW128F429D538.h5")
-y = DataGatherer("data/MillionSongSubset/A/R/G/TRARGBM128F428F65E.h5")
-z = DataGatherer("data/MillionSongSubset/A/L/A/TRALAKV12903CBD7F6.h5")
-t = DataGatherer("data/MillionSongSubset/B/H/H/TRBHHCU128F148AF19.h5")
-s = DataGatherer("data/MillionSongSubset/B/D/D/TRBDDCT12903CEF57E.h5")
-
-test = [x,y,z,t,s]
-df = pd.DataFrame
-songs = []
-def get_data(song_path):
-    song = DataGatherer(song_path)
-    song.read_file()
-
-    data = {
-        "hotttness":[song.get_song_hotttnesss()],
-        "key":[song.get_key()*song.get_key_confidence()], # the note or chord the music is centered around, the tonic
-        "mode":[song.get_mode()*song.get_mode_confidence()],   # type of scale with distinct melodic characteristics.
-        "fade_out": [song.get_start_of_fade_out()/song.get_duration()], # it is rare that some fade outs start later than the end
-        "fade_in": [song.get_end_of_fade_in()/song.get_duration()], 
-        "duration":[song.get_duration()],
-        "tempo":[song.get_tempo()], # este no lo ponemos mejor en music features
-        "time_signature":[song.get_time_signature()*song.get_time_signature_confidence()],
-        "analysis_sample_rate":[song.get_analysis_sample_rate()]
-    }
-
-    df = pd.DataFrame(data)
-    song.close_file()
-    return df
-
-# a = get_data("data/MillionSongSubset/A/B/A/TRABACN128F425B784.h5")
-# print(a)
-
-# for item in test:
-#     print(len(item.get_segments_()))
-
-# segments
-# tatum_start
-# sections
-
-
-#problems
-# a bar is one small segment of music that holds a number of beats.
-# bar               no lo pondría en general data
-
-# beat, in music, the basic rhythmic unit of a measure, or bar,
-# beats             no lo pondría en general data
-
-# In music, a section is a complete, but not independent, musical idea. 
-# Types of sections include the introduction or intro, exposition, 
-# development, recapitulation, verse, chorus or refrain, conclusion, 
-# coda or outro, fadeout, bridge or interlude.
-# get sections      no lo pondría en general data
-
-
-
-# artists_familiarity
-# analysis_sample_rate
-# artist_id
-# artist_latitude
-# artist_name
-# artist_longitude
-# artist_location
-# artist_mbid
-# artist_7digitalid
-# artist_playmeid
-# artists_terms_freq
-# audio_md5
-# release
-# release_7digitalid
-# song_id
-# title
-# 7digitalid
-# track_id
-# num_songs
-# similar_artists
-
-
-
+import numpy as np
 
 """
 This class process all the songs included in the millionsong
 dataset.
 """
-class Process():
+class Preprocess():
 
     def preprocess_general_data(file):
         """
-        Returns the song features prepared to include it in the dataset
+        Returns the song features related to the general data prepared to include them in the dataset
         """
         def get_data(song_path):
+            """
+            function that creates the structure of the dataframe with the features for the csv file
+            """
             song = DataGatherer(song_path)
             song.read_file()
 
@@ -114,16 +36,16 @@ class Process():
             }
 
             artist_terms = json.load(open("data/artist_terms.json"))
-            for i in range(len(x.get_artist_terms())):
-                term = x.get_artist_terms()[i]
-                term_freq = x.get_artist_terms_freq()[i]
-                term_weight =x.get_artist_terms_weight()[i]
+            for i in range(len(song.get_artist_terms())):
+                term = song.get_artist_terms()[i]
+                term_freq = song.get_artist_terms_freq()[i]
+                term_weight =song.get_artist_terms_weight()[i]
                 artist_terms["artist_term: "+str(term)[2:len(str(term))-1]] = term_freq*term_weight
             data.update(artist_terms)
 
             artist_mbtags = json.load(open("data/artist_mbtags.json"))
-            for i in range(len(y.get_artist_mbtags())):
-                mbtag = y.get_artist_mbtags()[i]
+            for i in range(len(song.get_artist_mbtags())):
+                mbtag = song.get_artist_mbtags()[i]
                 artist_mbtags["artist_mbtags: "+str(mbtag)[2:len(str(mbtag))-1]] = 1
             data.update(artist_mbtags)
 
@@ -140,5 +62,116 @@ class Process():
                         songs.append(get_data(f'data/MillionSongSubset/{a}/{b}/{c}/{d}'))
         df = pd.concat(songs)
         df.to_csv("data/general_data.csv",index=0)
-        return print("preprocess file")
+        return print("preprocessed general_data.csv file")
+
+    def preprocess_musical_data():
+        """
+        Returns the song features related to musical data prepared to include them in the dataset
+        """
+
+        def n_len_array(array,n):
+            """
+            function that converts any array in n length array (add 0 if the length if below n and combines if the length is greater than n)
+            """
+            narray = [0] * n
+            if len(array)<n:
+                for i in range(len(array)):
+                    narray[i] = array[i]
+                return narray
+            if len(array)>n:
+                times = int(len(array)/n)
+                for i in range(len(array)):
+                    narray[i%n] += array[i]/times
+                return narray
+            else:
+                return array
+
+        def include_feature(array,feature):
+            """
+            includes the array in a dictionary to add it to the data dictionary
+            """
+            feature_dict = {}
+            for i in range(len(array)):
+                feature_dict[feature+str(i)]=array[i]
+            return feature_dict
+
+
+        songs = []
+        def get_data(song_path):
+            """
+            function that creates the structure of the dataframe with the features for the csv file
+            """
+            song = DataGatherer(song_path)
+            song.read_file()
+
+            data = {
+                "hotttness":[song.get_song_hotttnesss()],
+                "key":[song.get_key()*song.get_key_confidence()],               # the note or chord the music is centered around, the tonic
+                "mode":[song.get_mode()*song.get_mode_confidence()],            # type of scale with distinct melodic characteristics.
+                "fade_out": [song.get_start_of_fade_out()/song.get_duration()], # it is rare that some fade outs start later than the end
+                "fade_in": [song.get_end_of_fade_in()/song.get_duration()], 
+                "duration":[song.get_duration()],
+                "tempo":[song.get_tempo()], 
+                "time_signature":[song.get_time_signature()*song.get_time_signature_confidence()],
+                "analysis_sample_rate":[song.get_analysis_sample_rate()]
+            }
+
+            segments_length = 400
+
+            segments_t = n_len_array(song.get_segments_timbre()[:,0]*song.get_segments_confidence(),segments_length)
+            segments_s = n_len_array(song.get_segments_start()*song.get_segments_confidence(),segments_length)
+            segments_l = n_len_array((song.get_segments_loudness_max_time()-song.get_segments_loudness_start())*song.get_segments_loudness_max()*song.get_segments_confidence(),segments_length)
+            
+            
+            segments_timbre = include_feature(segments_t,"segment_timbre_")
+            segments_start = include_feature(segments_s,"segment_start_")
+            segments_loudness =include_feature(segments_l,"segment_loudness_")
+
+            data.update(segments_timbre)
+            data.update(segments_start)
+            data.update(segments_loudness)
+
+            tatums_length= 500
+            tatums = n_len_array(song.get_tatums_start()*song.get_tatums_confidence(),tatums_length)
+            tatums_start = include_feature(tatums,"tatums_start_")
+            data.update(tatums_start)
+
+            sections_length = 10
+            sections_s = n_len_array(song.get_sections_start()*song.get_sections_confidence(),sections_length)
+            sections_start = include_feature(sections_s,"sections_start_")
+            data.update(sections_start)
+
+            bars_length=100
+            bars_s = n_len_array(song.get_bars_start()*song.get_bars_confidence(),bars_length)
+            bars_start = include_feature(bars_s,"bars_start_")
+            data.update(bars_start)
+
+            beats_length = 300
+            beats_s = n_len_array(song.get_beats_start()*song.get_beats_confidence(),beats_length)
+            beats_start = include_feature(beats_s,"beats_start_")
+            data.update(beats_start)
+
+            df = pd.DataFrame(data)
+            song.close_file()
+            return df
+
+        
+
+        df = pd.DataFrame
+        songs = []
+        for a in os.listdir('data/MillionSongSubset'):
+            for b in os.listdir('data/MillionSongSubset/%s/'%a):
+                for c in os.listdir('data/MillionSongSubset/%s/%s'%(a, b)):
+                    for d in os.listdir('data/MillionSongSubset/%s/%s/%s'%(a, b, c)):
+                        songs.append(get_data(f'data/MillionSongSubset/{a}/{b}/{c}/{d}'))
+
+
+        df = pd.concat(songs)
+        df.to_csv("data/musical_data.csv",index=0)
+        return print("preprocessed musical_data.csv file")
+
+if __name__ == "__main__":
+    preprocess = Preprocess()
+    preprocess.preprocess_general_data()
+    preprocess.preprocess_musical_data()
 
